0b73f4cd14ebc34ce5d5b464d5e2fb2989708737
digiwebdex <bditengineer@gmail.com>
2026-05-20 15:32:44 +0600

    Load invoice A4 sizing styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/invoice-a4.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
